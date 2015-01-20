4cde03cd0bdc1550dd1e56b34720925da30a2830
Nick Jones <nick@itsnotworking.co.uk>
2015-01-20 10:17:19 +0000

    Adding DirectAdmin install script

--- directadmin_install.sh
+++ directadmin_install.sh
@@ -13,7 +13,7 @@ if [ ! -f sync_dns_settings.py ]; then
       break;
     fi
   done
-  echo $'\n'"ip = \"$ip\""$'\n\n'"auth_token=\"$auth_token\"" >> sync_dns_settings.py
+  echo $'\n'"ip = \"$ip\""$'\n\n'"auth_token = \"$auth_token\"" > sync_dns_settings.py
   echo "Settings file created"
   #echo $auth_token
 fi
@@ -23,9 +23,14 @@ cp sync_dns.py /usr/local/directadmin/scripts/custom/
 cp sync_dns_settings.py /usr/local/directadmin/scripts/custom/
 echo "--> Done"
 
-echo $'\n'"Copying DirectAdmin custom scripts"
+echo $'\n'"Creating DirectAdmin custom scripts"
 if [ ! -f /usr/local/directadmin/scripts/custom/domain_post_create.sh ]; then
   # If the script doesn't exist, create the header
-  echo "#!/bin/bash"$'\n\n' >> /usr/local/directadmin/scripts/custom/domain_post_create.sh
+  echo "#!/bin/bash"$'\n\n' > /usr/local/directadmin/scripts/custom/domain_post_create.sh
 fi
-echo "python /usr/local/directadmin/scripts/custom/digitalocean-dns-sync/sync_dns.py \$domain" > /usr/local/directadmin/scripts/custom/domain_post_create.sh
+echo "python /usr/local/directadmin/scripts/custom/digitalocean-dns-sync/sync_dns.py \$domain" >> /usr/local/directadmin/scripts/custom/domain_post_create.sh
+echo "--> Done"
+
+echo $'\n'"Setting permissions"
+chmod -R diradmin:diradmin /usr/local/directadmin/scripts/custom/*
+echo "--> Done"
