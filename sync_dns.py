c69dce128e02dec921c7b946a7c41d38495ccbfa
Nick Jones <nick@itsnotworking.co.uk>
2016-02-16 20:16:13 +0000

    Updating sync script

--- sync_dns.py
+++ sync_dns.py
@@ -138,7 +138,7 @@ def sync_zone(domain_records_url, domain):
                     # Try and find an existing record
                     record_id = None
                     for record in existing_records:
-                        if type in ["CNAME", "MX", "NS"] and data != "@" and data[-1:] == ".":
+                        if type in ["CNAME", "MX", "NS"] and data[-1:] == ".":
                             check_data = data[:-1]
                         elif type == "CNAME" and data[-1:] != ".":
                             check_data = "{0}.{1}".format(data, domain)
@@ -152,7 +152,7 @@ def sync_zone(domain_records_url, domain):
                     if record_id:
                         print "--> Already exists, skipping"
                     else:
-                        if type in ["CNAME", "MX", "NS"] and data[-1:] != ".":
+                        if type in ["CNAME", "MX", "NS"] and data != "@" and data[-1:] != ".":
                             data = "{0}.{1}.".format(data, domain)
                         post_data = {
                             "type": type,
